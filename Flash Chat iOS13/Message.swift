306d71c79a21e210474e94fa1910033fcdbbe4b8
Jayam Verma <jayamverma@gmail.com>
2023-01-21 01:58:57 +0530

    Added Project files to the repository

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Jayam Verma on 16/01/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender:String
+    let body:String
+    
+}
