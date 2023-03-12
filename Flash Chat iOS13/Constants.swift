1f786cbedc81a5835ca4f31a0d26d95da7cdafdd
Jayam Verma <49724054+Jayamvr007@users.noreply.github.com>
2023-03-12 15:55:20 +0530

    Update Constants.swift

--- Flash Chat iOS13/Constants.swift
+++ Flash Chat iOS13/Constants.swift
@@ -1,10 +1,7 @@
 //
 //  Constants.swift
 //  Flash Chat iOS13
-//
-//  Created by Jayam Verma on 16/01/23.
-//  Copyright © 2023 Angela Yu. All rights reserved.
-//
+
 
 struct K {
     static let appName = "⚡️FlashChat"
